26cd4ba80bcb8b41340192804eaa293ac9a8612a
Stuhler <sophie.stuhler@wur.nl>
2017-01-11 11:12:54 +0100

    create a main.R file

---
+++ main.R
@@ -0,0 +1,17 @@
+# Sophie C. Stuhler
+# 11-01-2017
+# import packages
+
+library(raster)
+library(sp)
+library(spatstat)
+
+# Source functions
+
+source('')
+source('')
+
+# Load datasets
+load('')
+
+# Actual commands
